a90c92c653d0623a32105a326168897e96cd888a
l.borgato <l.borgato@newos.it>
2023-08-18 10:42:09 +0200

    first upload

--- src/App.js
+++ src/App.js
@@ -1,23 +1,114 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
+const PasswordErrorMessage = () => { 
+  return ( 
+    <p className="FieldError">Password should have at least 8 characters</p> 
+  ); 
+ }; 
+
 function App() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState({
+    value: "",
+    isTouched: false,
+  });
+  const [role, setRole] = useState("role");
+
+  const getFormValid = () => {
+    return (
+      firstName &&
+      email &&
+      password.length >= 8 &&
+      role !== "role"
+    );
+  };
+
+  const clearForm = () => {
+    setFirstName("");
+    setLastName("");
+    setEmail("");
+    setPassword({
+      value: "",
+      isTouched: "false"
+    });
+    setRole("role");
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Your account is now created!");
+    clearForm();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <h2>Sign Up</h2>
+          <div className="Field">
+            <label>First Name:</label>
+            <input type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="First Name:"
+            />
+          </div>
+          <div className="Field">
+            <label>Last Name:</label>
+            <input type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="Last Name:"
+            />
+          </div>
+          <div className="Field">
+            <label>Email:</label>
+            <input type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Email:"
+            />
+          </div>
+          <div className="Field"> 
+           <label> 
+             Password:
+           </label> 
+           <input 
+             value={password.value} 
+             type="password" 
+             onChange={(e) => { 
+               setPassword({ ...password, value: e.target.value }); 
+             }} 
+             onBlur={() => { 
+               setPassword({ ...password, isTouched: true }); 
+             }} 
+             placeholder="Password: " 
+           /> 
+           {password.isTouched && password.value.length < 8 ? ( 
+             <PasswordErrorMessage /> 
+           ) : null} 
+         </div> 
+          <div className="Field">
+            <label>Role:</label>
+            <select
+              value={role}
+              onChange={(e) => setRole(e.target.value)}
+              placeholder="Role:"
+            >
+              <option value="role">Role</option>
+              <option value="individual">Individual</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <button type="submit" disabled={!getFormValid()}>
+            Create Account
+          </button>
+        </fieldset>
+      </form>
     </div>
   );
 }
